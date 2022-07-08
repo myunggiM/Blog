847ce9b5e5e3a52aa7f0e4b9eac4173df4b0c9bb
myunggi_moon <myunggi_moon@naver.com>
2022-07-08 22:43:01 +0900

    JSX 활용 첫 페이지작성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import logo from './porkBelly.jpg';
 import './App.css';
 
 function App() {
+  let posts ='송파구 고기 맛집';
+  let postsCss = {color:'white' ,fontSize :'30px'};
+  function 함수(num){
+    return num
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <div className="black-nav"> {/*JSX 에서 class는 js , className 은 html,css */}
+        <div style={postsCss}>개발 Blog</div>
+      </div>
+      <h4>{posts} top { 함수(10) }</h4>
+      <img src={logo}></img>
     </div>
   );
 }
