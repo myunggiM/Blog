dfafb088ef2aa11c0a34d7d9df6541385ecfd79e
myunggi_moon <myunggi_moon@naver.com>
2022-07-19 23:00:35 +0900

    state가 array /object 일때

--- src/App.js
+++ src/App.js
@@ -16,11 +16,17 @@ function App() {
       <div className="black-nav"> {/*JSX 에서 class는 js , className 은 html,css */}
         <div style={postsCss}>ReactBlog</div>
       </div>
+      <button onClick={()=>{
+        let copy2 =[...글제목];
+        copy2= copy2.sort();
+        글제목변경(copy2);
+        console.log(copy2);
+      }}>가나다순정렬</button>
       <button onClick={() =>{
-         let copy =  글제목; 
+         let copy =  [...글제목]; {/* ... : 괄호 벗겨주세요. []: 괄호를 다시 씌워주세요 */}
          copy[0] = '여자코트 추천';
          글제목변경(copy);
-      }}> 글수정</button>
+      }}> 글수정</button> {/* state 변경함수 특징: 기존 state =신규 state 의 경우 변경 안해줌 : 글제목이란 변수 및 글제목이 가리키는 array 화살표 둘다 안바뀜 */}
       <div className="list">
         <h4>{ 글제목[0] }<span onClick={ () => { 좋아요변경(좋아요+1) }}>👍</span> { 좋아요 }</h4>  {/* onClick 안에는 함수만 가능하다. */}
         <p>7월 9일 발행</p>
